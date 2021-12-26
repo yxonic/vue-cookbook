80ab91c435773746a210281b39775bef69bca4ca
yxonic <yxonic@users.noreply.github.com>
2021-12-26 18:41:00 +0000

    deploy: b31124cc7b683653ecdb10c9f3e47562b8523c80

--- assets/01-assert-with-type-assertion.page.40bbf415.js
+++
@@ -1,9 +0,0 @@
-import{h as t}from"./vendor.1ba3453e.js";const e=[t("h1",{},["Assert with type assertions"]),t("pre",{},[`export function assert(
-  condition: boolean,
-  message?: string,
-): asserts condition {
-  if (!condition) {
-    throw new Error(message ?? 'assertion error')
-  }
-}
-`])];var r={render(){return e}};const s={title:"Assert with type assertion",layout:"book"};export{r as default,s as frontmatter,e as nodes};
