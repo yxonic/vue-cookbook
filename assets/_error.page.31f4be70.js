9c80dfa4d9b9e74f660c0b2cf78e86afb5294291
yxonic <yxonic@users.noreply.github.com>
2022-01-02 11:24:34 +0000

    deploy: d4dfbcb35861e57b12f6c301106d34bae6c70cdc

---
+++ assets/_error.page.31f4be70.js
@@ -0,0 +1,1 @@
+import{d,f as e,o,j as t,k as s}from"./vendor.d6a8e4f5.js";const _={key:0},c=t("h1",null,"404 Page Not Found",-1),r=s(" This page could not be found. "),a=[c,r],i={key:1},h=t("h1",null,"500 Internal Server Error",-1),l=s(" Something went wrong. "),p=[h,l],k=d({props:{is404:{type:Boolean}},setup(n){return(u,f)=>n.is404?(o(),e("div",_,a)):(o(),e("div",i,p))}});export{k as default};
